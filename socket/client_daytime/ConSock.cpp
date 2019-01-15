5a5c41e153920915b9749593c8c84b24143b07e5
windfornight <31735198+windfornight@users.noreply.github.com>
2019-01-15 17:21:15 +0800

    增加一些注释

--- socket/client_daytime/ConSock.cpp
+++ socket/client_daytime/ConSock.cpp
@@ -5,6 +5,39 @@
 
 #include "ConSock.h"
 
+/*
+*	struct hostent
+*	{
+*	char *h_name;  //主机名，即官方域名
+*	char **h_aliases;  //主机所有别名构成的字符串数组，同一IP可绑定多个域名
+*	int h_addrtype; //主机IP地址的类型，例如IPV4（AF_INET）还是IPV6
+*	int h_length;  //主机IP地址长度，IPV4地址为4，IPV6地址则为16
+*	char **h_addr_list;  // 主机的ip地址，以网络字节序存储。若要打印出这个IP，需要调用inet_ntoa()
+*	}
+
+*	struct servent {
+*	char  *s_name;		//服务名  
+*	char **s_aliases;	//服务别名列表
+*	int    s_port;		//端口号    
+*	char  *s_proto;		//使用的协议
+*	};
+
+*	struct protoent {
+*	char  *p_name;			//协议规范名
+*	char **p_aliases;		//主机的别名 
+*	int    p_proto;			//协议号
+*	};
+
+*	struct sockaddr_in {
+*	short int sin_family;			// 地址族 
+*	unsigned short int sin_port;	//端口号 
+*	struct in_addr sin_addr;		// Internet地址 
+*	unsigned char sin_zero[8];		// 与struct sockaddr一样的长度 
+*	};
+
+*/
+
+// localhost  daytime   tcp
 SOCKET connectsock(const char *host, const char *service, const char *transport)
 {
 	struct hostent *phe;  //pointer to host information entry
